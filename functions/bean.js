ba865874f197323289fc4be72f2c058e0671d848
Elukoye <lukoyedith@gmail.com>
2022-03-14 23:14:07 +0300

    second part soln for beans exercise

--- functions/bean.js
+++ functions/bean.js
@@ -1,11 +1,22 @@
-function countBs(word){
-    let result = [...word]
-    let counter = 0;
-    for(var i = 0; i < result.length; i++){
-        if(result[i] == result[i].toUpperCase()){
-            counter += 1;
+// function countBs(word){
+//     let counter = 0;
+//     for(var i = 0; i < result.length; i++){
+//         if(word.charAt(i) == result[i].toUpperCase()){
+//             counter += 1;
+//         }
+//     }
+//     return counter
+// };
+// console.log(countBs('BBC'));
+
+// part two
+function countChar( word,char){
+    let count = 0;
+    for(i = 0; i < word.length; i++){
+        if(word.charAt(i) == char){
+            count += 1;
         }
     }
-    return counter
+    return count;
 };
-console.log(countBs('BBC'));
+console.log(countChar("kakkerlak", "k"));
